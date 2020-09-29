7f0e12895b97f188b794aaaace9816f20957e663
Soumyadip Dutta <sdofficial247@gmail.com>
2020-09-29 19:55:38 +0530

    Netflix -Watch TV shows Online,Watch Movies Online
    I have build the Landing Page of famous web series platform Netflix . I have use HTML and CSS extensively and also javaScript

---
+++ main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+//Select tab content item
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  //Add  Border to Cuurent tab
+
+  this.classList.add("tab-border");
+
+  //Grab Content Item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  //Add show class
+
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
+
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
